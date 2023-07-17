28b318f43942ad26d526560183b241078ebc9cb5
MaryamTufail <maryamtufail78@gmail.com>
2023-07-17 17:39:52 -0400

    Update

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
       <h1 className="py-6 text-5xl leading-tight md:text-7xl md:leading-tight lg:text-8xl lg:leading-tight text-center">
       Level Up your Web Presence
       </h1>
-      <p className='pr-[19rem] w-full flex justify-end text-xl'>FrontendFirst, with its emphasis on modern designs and agile methods,  has<br/> proven to attract more customers than ever before. Don't hesitate any longer! <br/> Place your order now and experience the benefits firsthand.</p>
+      <p className='pr-[19rem] w-full flex justify-end text-xl'>FrontendFirst, with its emphasis on modern designs and agile methods,  has<br/> proven to attract more customers than ever before. Don&apos;t hesitate any longer! <br/> Place your order now and experience the benefits firsthand.</p>
       <a href='#'  data-target="project" className="transform px-10 py-2 border border-primary text-primary hover:bg-primary hover:text-secondary mt-[2.5rem]"  onClick={handleLinkClick}>
        Explore my journey
       </a>

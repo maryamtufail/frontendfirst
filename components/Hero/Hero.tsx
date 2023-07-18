5b51d1997ad00cc3e3305ee57acd58c53b3bd77c
MaryamTufail <maryamtufail78@gmail.com>
2023-07-18 14:49:49 -0400

    Fix: responsiveness

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = () => {
      <div className="py-6 text-4xl  md:text-7xl lg:text-8xl font-bold"><h1 >
       Level up your  </h1><p>Web <span className='text-conone'>Presence</span></p></div> 
       <p className='md:w-1/2 md:text-xl text-md'>FrontendFirst, with its emphasis on modern designs and agile methods,  has proven to attract more customers than ever before. Don&apos;t hesitate any longer! Place your order now and experience the benefits firsthand.</p>
-      <a href='#'  data-target="project" className="transform px-10 py-2 border border-primary text-primary hover:bg-primary hover:text-secondary mt-[3rem]"  onClick={handleLinkClick}>
+      <a href='#'  data-target="project" className="transform px-12 py-2 border border-primary text-primary hover:bg-primary hover:text-secondary mt-[3rem]"  onClick={handleLinkClick}>
        Explore my journey
       </a>
     </section>

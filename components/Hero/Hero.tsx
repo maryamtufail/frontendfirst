42b043d6ada50e9a95c0071237745f8979ab1aa7
MaryamTufail <maryamtufail78@gmail.com>
2023-07-18 14:46:07 -0400

    Issues: responsive

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -12,10 +12,10 @@ const Hero = () => {
   };
 
   return (
-    <section className="home flex flex-col items-center  w-full flex-col pt-[9rem] pb-[16rem] font-bold  text-center">
-     <div className="py-6 text-5xl leading-tight md:text-7xl md:leading-tight lg:text-8xl lg:leading-tight"><h1 >
+    <section className="home flex flex-col items-center  w-full flex-col pt-[9rem] pb-[14rem] px-3   text-center">
+     <div className="py-6 text-4xl  md:text-7xl lg:text-8xl font-bold"><h1 >
       Level up your  </h1><p>Web <span className='text-conone'>Presence</span></p></div> 
-      <p className='md:w-1/2 md:text-xl text-lg'>FrontendFirst, with its emphasis on modern designs and agile methods,  has proven to attract more customers than ever before. Don&apos;t hesitate any longer! Place your order now and experience the benefits firsthand.</p>
+      <p className='md:w-1/2 md:text-xl text-md'>FrontendFirst, with its emphasis on modern designs and agile methods,  has proven to attract more customers than ever before. Don&apos;t hesitate any longer! Place your order now and experience the benefits firsthand.</p>
       <a href='#'  data-target="project" className="transform px-10 py-2 border border-primary text-primary hover:bg-primary hover:text-secondary mt-[3rem]"  onClick={handleLinkClick}>
        Explore my journey
       </a>
--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -4,10 +4,10 @@ const Project = () => {
  
 
   return (
-    <section className="project px-2 py-20 bg-white flex flex-col justify-center">
-      <div className="py-10 text-center">
+    <section className="project px-2 py-16 bg-white flex flex-col justify-center">
+      <div className="py-5 text-center">
         <p className="text-2xl text-conone font-semibold">Work</p>
-        <h2 className="pt-2 text-4xl text-primary font-semibold">Embark on a journey through my creations.</h2>
+        <h2 className="pt-2 text-3xl md:text-4xl text-primary font-semibold">Embark on a journey through my creations.</h2>
         </div>
       <ProjectCard/>
     </section>

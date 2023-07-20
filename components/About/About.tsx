5c5f679e511fb642b5b5a4c5833eddccbcd286d9
MaryamTufail <maryamtufail78@gmail.com>
2023-07-20 02:19:36 -0400

    Fix: about

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const About = () => {
   return (
-    <section className="about flex md:flex-row flex-col bg-contwo items-center w-full justify-center py-20">
+    <section className="about flex md:flex-row flex-col bg-contwo items-center w-full justify-center py-[3rem]">
       <div className="flex md:w-1/4 justify-center">
         <Image
           src="/img/frontendabout.svg"
@@ -16,8 +16,8 @@ const About = () => {
       </div>
       <div className="md:w-1/2 w-full flex flex-col justify-center md:px-[8rem] py-[1rem] px-3">
         <h2 className="text-2xl font-bold mb-4 text-primary">About</h2>
-        <h3 className="text-3xl md:text-5xl font-semibold mb-4 text-conone">Frontend Developer</h3>
-        <p className="mb-9 md:text-lg text-md text-justify">
+        <h3 className="text-3xl md:text-4xl font-semibold mb-4 text-conone">Frontend Developer</h3>
+        <p className="mb-9 text-md text-justify">
           A mid-level Front-End Developer having 1.5+ years of experience, converting Figma designs into visually stunning and interactive user interfaces using the power of React.js and Next.js into pixel-perfect and interactive UIs. Some of my technical skills include HTML, CSS, Git, and Agile methodologies.
         </p>
         <DownloadResumeButton />

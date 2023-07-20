8a095b885d4b432953e84c23acd7357cb4232f3a
MaryamTufail <maryamtufail78@gmail.com>
2023-07-20 14:05:16 -0400

    logo change

--- components/Shared/Navbar/Navbar.tsx
+++ components/Shared/Navbar/Navbar.tsx
@@ -4,14 +4,14 @@ import { useState } from "react";
 import OutsideClickHandler from "react-outside-click-handler";
 
 export default function Navbar() {
- 
+
   const [offCanvas, setOffCanvas] = useState(false);
- 
+
   const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
     event.preventDefault();
     const targetClassName = event.currentTarget.getAttribute('data-target');
     const targetElements = document.querySelectorAll(`.${targetClassName}`);
-   
+
     if (targetElements.length > 0) {
       targetElements[0].scrollIntoView({ behavior: 'smooth' });
     }
@@ -20,7 +20,9 @@ export default function Navbar() {
   return (
     <>
       <nav className="flex justify-between items-center lg:px-[12rem] py-8 px-[1.5rem]">
-        <Image className="w-100 h-auto" src="/img/frontlogo.png" width={90} height={80} alt="logo"/>
+        <a href="#" data-target="home" onClick={handleLinkClick}>
+          <div className="text-lg uppercase">&lt; <span>Frontend</span><span className="text-conone"> first</span> /&gt;</div>
+        </a>
         <OutsideClickHandler onOutsideClick={() => setOffCanvas(false)}>
           <Image
             src="/img/menu.svg"
@@ -41,7 +43,7 @@ export default function Navbar() {
           />
         </div>
         <div className={style.canvas_content}>
-         
+
           <div className={style.menu}>
             <h4>Menu</h4>
             <p style={{ marginTop: 0 }}>
@@ -50,38 +52,38 @@ export default function Navbar() {
               </a>
             </p>
             <p>
-              <a href="#"  data-target="about" onClick={handleLinkClick}>
+              <a href="#" data-target="about" onClick={handleLinkClick}>
                 About
               </a>
             </p>
             <p>
-              <a href="#"  data-target="project" onClick={handleLinkClick}>
+              <a href="#" data-target="project" onClick={handleLinkClick}>
                 Projects
               </a>
             </p>
             <p>
-              <a href="#"  data-target="contact" onClick={handleLinkClick}>
+              <a href="#" data-target="contact" onClick={handleLinkClick}>
                 Contact
               </a>
             </p>
           </div>
         </div>
         <div className={style.social}>
-              <a href="https:www.instagram.com/frontendartist">
-                <Image src="/img/Instagram.svg"  width={32} height={32} alt="instagram"/>
-              </a>
-              <a href="https:www.linkedin.com/in/maryamtufail">
-                <Image src="/img/Linkedin.svg" width={32} height={32} alt="linkedin"/>
-               
-              </a>
-            </div>
+          <a href="https:www.instagram.com/frontendartist">
+            <Image src="/img/Instagram.svg" width={32} height={32} alt="instagram" />
+          </a>
+          <a href="https:www.linkedin.com/in/maryamtufail">
+            <Image src="/img/Linkedin.svg" width={32} height={32} alt="linkedin" />
+
+          </a>
+        </div>
         <div className={style.email}>
           <p>Get in touch</p>
           <h2>
             <u>maryamtufail78@gmail.com</u>
           </h2>
         </div>
-        
+
       </div>
     </>
   );

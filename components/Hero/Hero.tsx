939bed96863f1a22f4c6638c4870b3ad47127093
MaryamTufail <maryamtufail78@gmail.com>
2023-07-21 05:06:44 -0400

    Fix: responsive issue

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -12,10 +12,10 @@ const Hero = () => {
   };
 
   return (
-    <section className="home flex items-center w-full flex-col md:h-[70vh] h-[65vh] justify-center px-5 text-center">
-     <div className="py-6 text-4xl  md:text-7xl lg:text-8xl font-bold"><h1 >
-      Level up your  </h1><p>Web <span className='text-conone'>Presence</span></p></div> 
-      <p className='md:w-1/2 md:text-xl text-md'>FrontendFirst is dedicated to boosting your online presence with cutting-edge designs and agile methodologies. Don&apos;t miss out on the opportunity to enhance your business. Take action now and witness the remarkable benefits firsthand.</p>
+    <section className="home flex items-center w-full flex-col justify-center md:px-[15rem] px-10 md:py-[9rem] py-[3rem] text-center">
+     <div className="py-6 text-4xl md:text-7xl  font-bold"><h1 >
+      Level up your Web<span className='text-conone'> Presence</span></h1></div> 
+      <p className='md:text-xl text-md'>FrontendFirst is dedicated to boosting your online presence with cutting-edge designs and agile methodologies. Don&apos;t miss out on the opportunity to enhance your business. Take action now and witness the remarkable benefits firsthand.</p>
       <a href='#'  data-target="project" className="transition delay-150 px-12 py-2 border border-primary text-primary hover:bg-primary hover:text-secondary mt-[3rem]"  onClick={handleLinkClick}>
        Explore my journey
       </a>

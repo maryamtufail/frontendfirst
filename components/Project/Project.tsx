aed10dad9dff7c18d3eff48b954175298a30739a
MaryamTufail <maryamtufail78@gmail.com>
2023-07-21 10:58:06 -0400

    Project animate

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
-import {Zoom } from "react-awesome-reveal"
+import {Zoom, Slide } from "react-awesome-reveal"
 
 const Project = () => {
  
@@ -8,8 +8,8 @@ const Project = () => {
   return (
     <section className="project md:px-[10rem] py-[4rem] bg-white flex flex-col justify-center text-center">
       <div className="py-5 text-center">
-        <p className="text-2xl text-conone font-semibold">Work</p>
-        <h2 className="pt-4 pb-6 px-2 text-2xl md:text-4xl text-primary font-semibold">Embark on a journey through my creations.</h2>
+        <Slide  className="text-2xl text-conone font-semibold">Work</Slide>
+        <Slide className=" px-2 text-2xl md:text-4xl text-primary font-semibold">Embark on a journey through my creations.</Slide>
         </div>
         <Zoom triggerOnce> <ProjectCard/></Zoom>
    

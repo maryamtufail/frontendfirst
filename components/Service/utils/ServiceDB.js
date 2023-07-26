9b424d89299e2cc25f99bc96382a50ba3f41d8fe
MaryamTufail <maryamtufail78@gmail.com>
2023-07-26 19:38:18 -0400

    service: icons change

--- components/Service/utils/ServiceDB.js
+++ components/Service/utils/ServiceDB.js
@@ -1,16 +1,16 @@
 export const ServiceDB = [
   {
-    image: "/img/figmatocode.png",
+    image: "/img/figmaatocode.png",
     title: "Figma to Code",
     description: "Transform your Figma designs into pixel-perfect code with the latest frontend technologies.",
   },
   {
-    image: "/img/ideatocode.png",
+    image: "/img/ideastocode.png",
     title: "Idea to Code",
     description: "Have a brilliant idea? turn it into reality with the latest frontend technologies.",
   },
   {
-    image: "/img/redesign.png",
+    image: "/img/redesignweb.png",
     title: "Redesign a Website",
     description: "Give your website a fresh look and improved user experience by using new technologies. ",
   },

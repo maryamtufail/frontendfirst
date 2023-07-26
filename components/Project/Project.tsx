3fe28c5425665581cb6b93953a1a0f68155b1bfa
MaryamTufail <maryamtufail78@gmail.com>
2023-07-26 19:02:29 -0400

    Project section edit

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -5,10 +5,10 @@ const Project = () => {
  
 
   return (
-    <section className="project md:px-[5rem] py-[4rem] bg-white flex flex-col justify-center text-center">
-      <div className="py-5 text-center">
-        <Slide  triggerOnce className="text-2xl text-conone font-semibold"><p>Work</p></Slide>
-        <Slide triggerOnce className=" px-2 pb-2 text-2xl md:text-4xl text-primary font-semibold"><p>Embark on a journey through my creations.</p></Slide>
+    <section className="project md:px-[5rem] py-[4rem] bg-white flex flex-col justify-center">
+      <div className="py-5">
+        <Slide  triggerOnce className="text-2xl text-conone font-semibold"><p>Project</p></Slide>
+        <Slide triggerOnce className=" pb-2 text-2xl md:text-4xl text-primary font-semibold"><p>Embark on a journey through my creations.</p></Slide>
         </div>
        <ProjectCard/>
    
--- components/Project/ProjectCard.tsx
+++ components/Project/ProjectCard.tsx
@@ -1,44 +1,25 @@
 import { Projects } from "./utils/ProjectDB";
 import Image from "next/image";
-import {Zoom } from "react-awesome-reveal"
+import { Zoom } from "react-awesome-reveal"
 const ProjectCard = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      <Zoom triggerOnce > 
-      {Projects.map((item) => (
-        <div key={item.title} className="relative bg-white shadow-md rounded-lg overflow-hidden">
-          <div className="h-48 sm:h-60 md:h-72 relative">
-            <Image
-              src={item.image}
-              alt={item.title}
-              layout="fill"
-              objectFit="cover"
-            
-              className="transform transition-transform duration-300 hover:scale-110"
-            />
-          </div>
-          <div className="p-4">
-            <h3 className="text-lg sm:text-xl font-bold text-conone">{item.title}</h3>
-            <p className="text-base py-4 pb-6">{item.description}</p>
-            <div className="flex flex-wrap justify-center mb-4">
-              {item.skills.map((skill) => (
-                <span key={skill} className="inline-block bg-contwo text-primary font-bold text-sm px-4 py-1 mr-2 mb-2">
-                  {skill}
-                </span>
-              ))}
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+      <Zoom triggerOnce >
+        {Projects.map((item) => (
+          <div key={item.title} className="relative bg-white shadow-md rounded-lg overflow-hidden">
+            <div className="h-40 sm:h-56 md:h-[12rem] relative">
+              <Image
+                src={item.image}
+                alt={item.title}
+                width={500}
+                height={500}
+                className="transform transition-transform duration-300 hover:scale-110"
+              />
             </div>
-            <div className="flex justify-center space-x-4">
-              <a href={item.link} className="border-2 border-primary block text-center bg-primary text-secondary font-bold py-2 px-4 rounded-full transition duration-300 hover:bg-transparent hover:text-primary">
-                See View
-              </a>
-              <a href={item.link} className="border-2 border-primary block text-center text-primary font-bold py-2 px-4 rounded-full transition duration-300 hover:text-secondary hover:bg-primary">
-                GitHub
-              </a>
-            </div>
+           
           </div>
-        </div>
-      ))}
-      </Zoom> 
+        ))}
+      </Zoom>
     </div>
   );
 };

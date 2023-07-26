65dad236deafd1a73792e15ed97c34c58ee70c80
MaryamTufail <maryamtufail78@gmail.com>
2023-07-26 17:21:08 -0400

    Update: About

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,25 +1,26 @@
 import DownloadResumeButton from "../Shared/DResume/DownloadResumeButton";
 import Image from "next/image";
 import Typewriter from "typewriter-effect";
-import {Zoom } from "react-awesome-reveal"
+import { Zoom } from "react-awesome-reveal";
+import { FaCheckCircle } from "react-icons/fa";
+
 const About = () => {
   return (
-    <section className="about flex md:flex-row flex-col bg-contwo items-center w-full justify-center py-[10rem]">
+    <section className="about flex md:flex-row flex-col bg-contwo items-center w-full justify-center py-[8rem]">
       <Zoom triggerOnce className="flex md:w-1/4 justify-center">
-     <div>
-        <Image
-          src="/img/about.png"
-          alt="About"
-          className="object-fit w-[50rem] h-auto"
-          width={1070}
-          height={1070}
-        />
+        <div>
+          <Image
+            src="/img/about.png"
+            alt="About"
+            className="object-fit w-[50rem] h-auto"
+            width={1070}
+            height={1070}
+          />
         </div>
       </Zoom>
       <div className="md:w-1/2 w-full flex flex-col justify-center md:px-[8rem] py-[1rem] px-3">
-        <h2 className="text-2xl font-bold mb-4 text-primary">About</h2>
         <h3 className="text-3xl md:text-4xl font-semibold mb-4 text-conone">
-        <Typewriter
+          <Typewriter
             options={{
               strings: ["Frontend Developer", "Tea Lover", "Freelancer"],
               autoStart: true,
@@ -27,9 +28,27 @@ const About = () => {
             }}
           />
         </h3>
-        <p className="mb-9 text-md text-justify">
-        Hello, I'm Maryam Tufail, a Mid-Level Front-End Developer with over 2 years of experience in crafting responsive UIs to drive business growth. My main objective is to continuously enhance my skills, enabling me to elevate customer engagement and establish a strong online presence that fosters business expansion.
+        <p className="mb-2 text-md text-justify">
+          Hello, I'm Maryam Tufail, a Mid-Level Front-End Developer with over 2
+          years of experience in crafting responsive UIs to drive business
+          growth. My main objective is to continuously enhance my skills,
+          enabling me to elevate customer engagement and establish a strong
+          online presence that fosters business expansion.
         </p>
+        <div className="mb-5">
+          <p>
+            <FaCheckCircle className="w-5 h-5 inline mr-2 my-2" />
+            Pixel-Perfect Web Pages
+          </p>
+          <p>
+            <FaCheckCircle className="w-5 h-5 inline mr-2 my-2" />
+            Efficient Project Delivery
+          </p>
+          <p>
+            <FaCheckCircle className="w-5 h-5 inline mr-2 my-2" />
+            Performance Optimization
+          </p>
+        </div>
         <DownloadResumeButton />
       </div>
     </section>

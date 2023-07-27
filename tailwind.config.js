ed6934df4c2988650a7c254ddb1a03edb1f0399f
MaryamTufail <maryamtufail78@gmail.com>
2023-07-27 14:38:32 -0400

    color change

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
       colors: {
         primary: '#1d1d1d', // Set your primary color
         secondary: '#f6f6f6', // Set your secondary color
-        conone: '#fe924c',
+        conone: '#8F3A00',
         contwo: '#f0f0ec',
       },
       backgroundColor: {

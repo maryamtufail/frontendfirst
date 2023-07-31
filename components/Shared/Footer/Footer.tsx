db9d4494a913c53e7b991277ccac359ccba0a579
MaryamTufail <maryamtufail78@gmail.com>
2023-07-31 17:42:14 -0400

    Footer design

--- components/Shared/Footer/Footer.tsx
+++ components/Shared/Footer/Footer.tsx
@@ -1,45 +1,18 @@
+import {  FiInstagram, FiLinkedin, FiMail } from 'react-icons/fi';
 
 const Footer = () => {
-  const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
-    event.preventDefault();
-    const targetClassName = event.currentTarget.getAttribute('data-target');
-    const targetElements = document.querySelectorAll(`.${targetClassName}`);
-    if (targetElements.length > 0) {
-      targetElements[0].scrollIntoView({ behavior: 'smooth' });
-    }
-  };
 
   return (
-    <footer className="bg-gray-200 py-12">
-      <div className=" mx-auto px-2 flex flex-col items-center justify-center">
-        <div className="mb-6 uppercase text-conone ">
-          <a
-            href="#"
-            data-target="about"
-            className="mr-4 hover:text-primary"
-            onClick={handleLinkClick}
-          >
-            About
-          </a>
-          <a
-            href="#"
-            data-target="project"
-            className="mr-4 hover:text-primary"
-            onClick={handleLinkClick}
-          >
-            Projects
-          </a>
-          <a
-            href="#"
-            data-target="contact"
-            className="hover:text-primary"
-            onClick={handleLinkClick}
-          >
-            Contact
-          </a>
+    <footer className="bg-primary p-[3rem] text-secondary">
+      <div className="w-full flex md:flex-row flex-col px-7 py-[4rem] mx-auto  rounded-lg bg-secondary ">
+        <div className="w-full md:w-1/2 mb-12 md:px-[4rem] ">
+          <h3 className="text-2xl md:text-5xl font-bold">Let&apos;s Meet for a Brown Tea <br />Chat - Virtually</h3>
+          <p className="text-md mt-4">  &copy; {new Date().getFullYear()} FrontendFirst. All rights reserved.</p>
+          <p className="text-sm"> Design and Developed by your trusty </p>
         </div>
-        <div className="text-gray-700">
-          &copy; {new Date().getFullYear()} FrontendFirst. All rights reserved.
+        <div className="flex flex-col w-full md:w-1/2 justify-center text-lg md:text-4xl font-bold gap-2">
+        <a className='flex items-center' href="/"><FiInstagram className=' mr-2 inline md:w-8 h-8'/> Instagram</a>
+        <a className='flex items-center' href="/"> <FiLinkedin className=' mr-2 inline md:w-8 h-8'/> LinkedIn</a>
         </div>
       </div>
     </footer>

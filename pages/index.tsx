bd1df5739b70d8fda8c85a6931939ac985e8cc10
MaryamTufail <maryamtufail78@gmail.com>
2023-07-31 16:35:06 -0400

    build error resolve

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Shared/Navbar/Navbar";
 import Hero from "../components/Hero/Hero";
 import About from "../components/About/About";
-import Project from "../components/Project/Project";
+// import Project from "../components/Project/Project";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Shared/Footer/Footer";
 import Skill from "../components/Skill/Skill";
@@ -20,7 +20,7 @@ const Home = () => {
    <DifferMe/>
    <WhyMe/>
    <Testimonial/>
-   <Project/>
+   {/* <Project/> */}
    <Contact/>
    <Footer/>
     </div>

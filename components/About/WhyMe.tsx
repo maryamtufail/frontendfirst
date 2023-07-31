946fdad43b362457a22f62b3e9efce1ff77857b0
MaryamTufail <maryamtufail78@gmail.com>
2023-07-31 17:02:06 -0400

    build error resolve

--- components/About/WhyMe.tsx
+++ components/About/WhyMe.tsx
@@ -16,7 +16,7 @@ const WhyMe = () => {
             <div className="max-w-4xl mx-auto">
             <Slide triggerOnce className="py-6 text-4xl md:text-6xl  font-bold"> <h1>Your Helping Hand for growth!</h1></Slide>
                 <Slide triggerOnce className='md:text-lg text-md mt-5'><p className="mb-3">First off, I want to say that I&apos;m really excited to connect with all of you. We are in same boat as a small front-end developer business owner for quite some time now. </p>
-                    <p className="mb-3">It&apos;s disheartening to see hardworking entrepreneurs like you struggling with websites that just don't deliver the results you deserve.</p>
+                    <p className="mb-3">It&apos;s disheartening to see hardworking entrepreneurs like you struggling with websites that just don&apos;t deliver the results you deserve.</p>
                     <p className="mb-3">if you&apos;re ready to take your business to the next level and stop searching &quot;how to build a website&quot;.</p>
                     <p> Here&apos;s the deal - I genuinely want to help you succeed and make a positive impact with your online presence.</p>
                 </Slide></div>

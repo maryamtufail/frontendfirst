3ad776a9567471418d790b2422258c1320eeab29
MaryamTufail <maryamtufail78@gmail.com>
2023-07-31 09:23:26 -0400

    build error resolve

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className='flex flex-col py-6 text-secondary'>
           <div className="text-lg uppercase mt-1 mb-6 font-bold">&lt; <span>Frontend</span><span className="text-accent"> first</span> /&gt;</div>
-            <h1 className="text-xl md:text-2xl font-bold">Got a Project in Mind, Let's Connect!</h1>
+            <h1 className="text-xl md:text-2xl font-bold">Got a Project in Mind, Let&apos;s Connect!</h1>
             <div className="flex items-center gap-2 justify-center mt-4">
               <FiMail className=" text-xl" />
               <p className="">maryamtufail78@gmail.com</p>
